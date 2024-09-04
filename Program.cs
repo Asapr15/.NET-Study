9c011640e373ee29dd8a28194a68769496e9a53d
Asapr15 <alisson.asap@gmail.com>
2024-09-04 17:50:37 -0300

    Realizando a leitura de um arquivo

--- Program.cs
+++ Program.cs
@@ -1,22 +1,51 @@
 ﻿using _NET_STUDY.Models;
 using System.Globalization;
 
-string dataString = "2022-04-17 18:00";
+string[] linhas= File.ReadAllLines("Arquivos/arquivoLeitura.txt");
 
-bool sucesso = DateTime.TryParseExact(dataString,
-                      "yyyy-MM-dd HH:mm",
-                       CultureInfo.InvariantCulture,
-                       DateTimeStyles.None, 
-                       out DateTime data);
+foreach(string linha in linhas)
+{
+    Console.WriteLine(linha);
+}
 
-if (sucesso)
-{
-    Console.WriteLine($"Conversão com sucesso! Data: {data}");
-}
-else
-{
-    Console.WriteLine($"{dataString} não é uma data válida");
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// string dataString = "2022-04-17 18:00";
+
+// bool sucesso = DateTime.TryParseExact(dataString,
+//                       "yyyy-MM-dd HH:mm",
+//                        CultureInfo.InvariantCulture,
+//                        DateTimeStyles.None, 
+//                        out DateTime data);
+
+// if (sucesso)
+// {
+//     Console.WriteLine($"Conversão com sucesso! Data: {data}");
+// }
+// else
+// {
+//     Console.WriteLine($"{dataString} não é uma data válida");
+// }
 
 
 
